3c1cdf4e40702a36a4383602f69c30a50d7a8aad
Albert Pi <albertpi@gmail.com>
2012-04-11 21:40:34 +0200

    Grappelli inline ordering added

--- admin.py
+++ admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from bohbou.web.models import *
 from sorl.thumbnail.admin import AdminImageMixin
 
+class FeaturedSlideOptions(admin.TabularInline):
+    fields = ('position', 'status', 'caption', 'description', 'link', 'created',)
+    # define the sortable
+    sortable_field_name = "position"
 
-class FeaturedAdmin(AdminImageMixin, admin.ModelAdmin):
+class FeaturedAlbumAdmin(AdminImageMixin, admin.ModelAdmin):
     list_display = ('status', 'caption', 'link', 'created')
+
 
 admin.site.register(Featured, FeaturedAdmin)
--- models.py
+++ models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from sorl.thumbnail import ImageField
 
-from bohbou.web.managers import PublicManager
 
-class Featured(models.Model):
+from managers import PublicManager
+
+class FeaturedSlide(models.Model):
     """
     (Featured description)
     """
@@ -15,16 +16,20 @@ class Featured(models.Model):
     )
     
     caption = models.CharField(_('caption'), blank=True, max_length=255)
+    description = models.TextField(blank=True)
     image  = ImageField(_('picture'), upload_to='teasers', blank=True,)
     link    = models.URLField(blank=True, verify_exists=True)
     status  = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=2)
+    # position field
+    position = models.PositiveSmallIntegerField("Position")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     objects = PublicManager()
     
     class Meta:
-        verbose_name_plural = 'Featured'
-        ordering = ('-created',)
+        verbose_name = u'Featured Slide'
+        verbose_name_plural = 'Featured Slides'
+        ordering = ('position',)
     
     
     # If using the get_absolute_url method, put the following line at the top of this file:
@@ -34,4 +39,14 @@ class Featured(models.Model):
     def get_absolute_url(self):
         return ('featured_detail_view_name', [str(self.id)])
     
+class FeaturedAlbum(models.Model):
+    """Image gallery for slider"""
+    name = models.CharField(max_length=150)
+    images = models.ManyToManyField(SliderImage, blank=True)
 
+    class Meta:
+        verbose_name = u'Featured Album'
+        verbose_name_plural = u'Featured Albums'
+
+    def __unicode__(self):
+        return self.name
